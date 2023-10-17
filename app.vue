c9f3b8a26c2bd4560662d5f328a880abc48541db
zhukov <zhukov@etalon48.com>
2023-10-17 10:38:29 +0300

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup>
+const user = useSupabaseUser();
+</script>
+
+<template>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Account v-if="user" />
+    <Auth v-else />
+  </div>
+</template>
---
+++ components/Auth.vue
@@ -0,0 +1,55 @@
+<script setup>
+const supabase = useSupabaseClient();
+
+const loading = ref(false);
+const email = ref("");
+const password = ref("");
+
+const handleLogin = async () => {
+  try {
+    loading.value = true;
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email.value,
+      password: password.value,
+    });
+    if (error) throw error;
+  } catch (error) {
+    alert(error.error_description || error.message);
+  } finally {
+    loading.value = false;
+  }
+};
+</script>
+
+<template>
+  <form class="row flex-center flex" @submit.prevent="handleLogin">
+    <div class="col-6 form-widget">
+      <h1 class="header">Авторизация</h1>
+      <p class="description">Введите логин и пароль в поля ниже</p>
+      <div>
+        <input
+          class="inputField"
+          type="email"
+          placeholder="Your email"
+          v-model="email"
+        />
+      </div>
+      <div>
+        <input
+          class="inputField"
+          type="password"
+          placeholder="password"
+          v-model="password"
+        />
+      </div>
+      <div>
+        <input
+          type="submit"
+          class="button block"
+          :value="loading ? 'Загрузка' : 'Авторизоваться'"
+          :disabled="loading"
+        />
+      </div>
+    </div>
+  </form>
+</template>
